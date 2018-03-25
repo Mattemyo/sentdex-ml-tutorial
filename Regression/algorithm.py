cf87e3c2237536cafb71bea09ae5cdced4a04aeb
Mattemyo <matias97marti@gmail.com>
2018-03-25 21:56:05 +0200

    print r_squared

--- Regression/algorithm.py
+++ Regression/algorithm.py
@@ -14,8 +14,8 @@ ys = np.array([5, 4, 6, 5, 6, 7])
 
 def best_fit_slope_and_intercept(xs, ys):
     m = (
-        (mean(xs)*mean(ys) - mean(xs*ys)) /
-        (mean(xs)**2 - mean(xs**2))
+        ((mean(xs)*mean(ys)) - mean(xs*ys)) /
+        ((mean(xs))**2 - mean(xs**2))
     )
 
     b = mean(ys) - m * mean(xs)
@@ -28,7 +28,7 @@ def squared_error(ys_orig, ys_line):
 
 
 def coefficient_of_determination(ys_orig, ys_line):
-    y_mean_line = [mean(ys_orig) for y in ys_orig]
+    y_mean_line = mean(ys_orig)
     squared_error_regr = squared_error(ys_orig, ys_line)
     squared_error_y_mean = squared_error(ys_orig, y_mean_line)
     return 1 - (squared_error_regr / squared_error_y_mean)
