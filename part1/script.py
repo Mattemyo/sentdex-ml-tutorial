7ac93dd695bd9c4c4d3168598ad7feacb3ebef7f
Mattemyo <matias97marti@gmail.com>
2018-03-25 11:48:34 +0200

    train and test first model

--- part1/script.py
+++ part1/script.py
@@ -1,6 +1,8 @@
 import pandas as pd
-import quandl
-import math
+import quandl, math
+import numpy as np
+from sklearn import preprocessing, cross_validation, svm
+from sklearn.linear_model import LinearRegression
 
 df = quandle.get('WIKI/GOOGL')
 
@@ -21,3 +23,15 @@ df['label'] = df[forecast_col].shift(-forecast_out)
 
 df.dropna(inplace=True)
 
+X = np.array(df.drop(['label'],1))
+y = np.array(df['label'])
+X = preprocessing.scale(X)
+y = np.array(df['label'])
+
+X_train, X_test, y_train, y_test = cross_validation.train_test_split(X,y,test_size=0.2)
+
+
+clf = LinearRegression(n_jobs=-1)
+clf.fit(X_train, y_train)
+
+accuracy = clf.score(X_test, y_test)
