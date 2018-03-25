6d5525bb09ff7b2d0598495ac9cee18e1b1d4275
Mattemyo <matias97marti@gmail.com>
2018-03-25 17:43:17 +0200

    rename directory and file, create algorithm file

--- Regression/implementation.py
+++ Regression/implementation.py
@@ -50,11 +50,12 @@ X_train, X_test, y_train, y_test = model_selection.train_test_split(
 clf = LinearRegression(n_jobs=-1)
 clf.fit(X_train, y_train)
 
-with open('linearregression.pickle','wb') as f:
-  pickle.dump(clf, f)
+with open('linearregression.pickle', 'wb') as f:
+    pickle.dump(clf, f)
 
 pickle_in = open('linearregression.pickle', 'rb')
 clf = pickle.load(pickle_in)
+
 
 accuracy = clf.score(X_test, y_test)
 forecast_set = clf.predict(X_lately)
@@ -69,9 +70,9 @@ next_unix = last_unix + one_day
 
 
 for i in forecast_set:
-  next_date = datetime.datetime.fromtimestamp(next_unix)
-  next_unix +=one_day
-  df.loc[next_date] = [np.nan for _ in range(len(df.columns)-1)] + [i]
+    next_date = datetime.datetime.fromtimestamp(next_unix)
+    next_unix += one_day
+    df.loc[next_date] = [np.nan for _ in range(len(df.columns)-1)] + [i]
 
 df['Adj. Close'].plot()
 df['Forecast'].plot()
