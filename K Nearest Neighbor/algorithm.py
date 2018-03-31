3612e677f34aea0def8854441063c935f9517351
Mattemyo <matias97marti@gmail.com>
2018-03-31 13:07:04 +0200

    compare own alg with sklearn's radius alg

--- K Nearest Neighbor/algorithm.py
+++ K Nearest Neighbor/algorithm.py
@@ -21,44 +21,54 @@ def k_nearest_neighbors(data, predict, k=3):
     votes = [i[1] for i in sorted(distances)[:k]]
 
     vote_result = Counter(votes).most_common(1)[0][0]
-    return vote_result
+    confidence = Counter(votes).most_common(1)[0][1] / k
+    return vote_result, confidence
 
 
-df = pd.read_csv('tumour_data.txt')
-df.replace('?', -99999, inplace=True)
-df.drop(['id'], 1, inplace=True)
-full_data = df.astype(float).values.tolist()
+accuracies = []
 
-random.shuffle(full_data)
+for i in range(5):
+
+    df = pd.read_csv('tumour_data.txt')
+    df.replace('?', -99999, inplace=True)
+    df.drop(['id'], 1, inplace=True)
+    full_data = df.astype(float).values.tolist()
+
+    random.shuffle(full_data)
+
+    test_size = 0.4
+    train_set = {
+        2: [],
+        4: []
+    }
+    test_set = {
+        2: [],
+        4: []
+    }
+
+    train_data = full_data[:-int(test_size*len(full_data))]
+    test_data = full_data[-int(test_size*len(full_data)):]
+
+    for i in train_data:
+        train_set[i[-1]].append(i[:-1])
+
+    for i in test_data:
+        test_set[i[-1]].append(i[:-1])
+
+    correct = 0
+    total = 0
+
+    for group in test_set:
+        for data in test_set[group]:
+            vote, confidence = k_nearest_neighbors(train_set, data, k=75)
+            if group == vote:
+                correct += 1
+            else:
+                # print(confidence)
+            total += 1
+
+    #print('Accuracy:', correct/total)
+    accuracies.append(correct/total)
 
 
-test_size = 0.2
-train_set = {
-    2: [],
-    4: []
-}
-test_set = {
-    2: [],
-    4: []
-}
-
-train_data = full_data[:-int(test_size*len(full_data))]
-test_data = full_data[-int(test_size*len(full_data)):]
-
-for i in train_data:
-    train_set[i[-1]].append(i[:-1])
-
-for i in test_data:
-    test_set[i[-1]].append(i[:-1])
-
-correct = 0
-total = 0
-
-for group in test_set:
-    for data in test_set[group]:
-        vote = k_nearest_neighbors(train_set, data, k=5)
-        if group == vote:
-            correct += 1
-        total += 1
-
-print('Accuracy:', correct/total)
+print(sum(accuracies)/len(accuracies))
